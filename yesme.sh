b4ef2c68437a5ba18694a98b5eef19277de0df33
Dan Barrett <dan@theblahman.net>
2012-05-18 21:51:44 +1000

    Added comments inside script.

--- yesme.sh
+++ yesme.sh
@@ -11,6 +11,7 @@
 # has before running multiple sessions of yes
 # for each core.
 
+# Function that cleans the yes sessions before exiting
 control_c()
 {
 	echo "\n*** Exiting… ***"
@@ -18,24 +19,33 @@ control_c()
 	exit $?
 }
 
+# Hook so if the script is quited, kill the yes'
 trap control_c SIGINT
 
+# Get the number of cores from System Profiler/Information
 info=`system_profiler SPHardwareDataType | grep 'Cores:'`
+# Start the string at the core numbers
 info=${info:29}
+# Remove any whitespace at the end
 info=`echo $info | sed 's/ *$//g'`
+# Add a extra core because...
 info=$[$info+1]
 
+# Set our loop var
 i="1"
 
 while [ $i -lt $info ]
 	do
 	echo "Stressing Core: $i"
+	# Start a yes session in the background
 	yes > /dev/null &
 	i=$[$i+1]
 done
 
 echo "Press Control + C to quit script and kill yes"
 
+# Adding this while means the script doesn't return
+# straight after running
 while true;
 	do
 	read x;
