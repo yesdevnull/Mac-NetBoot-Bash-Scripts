74ebb223803afc38646d7207b1a0f30825dbb0b3
Dan Barrett <dan@theblahman.net>
2012-05-18 21:29:40 +1000

    Initial commit of memtester.sh

---
+++ memtester.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 99% of the time, disk0s2 is the Mac boot volume
+disk=`diskutil list | grep 'disk0s2'`
+# Get the stuff between partition type and size
+disk=${disk:33:24}
+# Remove any whitespace from end of name
+disk=`echo $disk | sed 's/ *$//g'`
+# If there's spaces in the disk name, add an escape slash
+disk=`echo $disk | sed -e 's/ /\\\ /g'`
+
+finalPath='/Volumes/'$disk'/usr/bin';
+
+# Copy memtest to users HDD
+sudo cp /Applications/memtest "$finalPath"
+
+# Write our mt alias
+echo "alias mt='memtest all -L 3'" | sudo tee -a /etc/profile
+# Writes an alias to PRAM reset on reboot
+echo "alias sreboot='nvram boot-args=\"-p -r\"'; reboot" | sudo tee -a /etc/profile
+
+# Make our mt alias be run at root login
+echo "echo ------- Memory Test -------" | sudo tee -a /var/root/.profile
+echo "mt" | sudo tee -a /var/root/.profile
+echo "echo ---- Ended Memory Test ----" | sudo tee -a /var/root/.profile
+
+# Sets bootup to Single User Mode
+sudo nvram boot-args="-s"
+
+# Reboot
+sudo reboot
